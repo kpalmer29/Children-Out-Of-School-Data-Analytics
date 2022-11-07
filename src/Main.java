9a583f09d00aa12b41ce9e0fe4752aa7ed62d15b
Kavi Palmer <kavipalmer@Kavis-MacBook-Pro.local>
2022-11-07 07:50:19 -0600

    Significant Changes Methods

--- src/Main.java
+++ src/Main.java
@@ -8,25 +8,42 @@ import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class Main {
 
+    //HashMap containing data read from .csv files
     private static HashMap<String, ArrayList<Integer>> countryMap;
+
+    //list countries who report no data before 1989
     private static ArrayList<String> nonReporters;
+
+    //Map of countries who show significant improvement (-5% from lowest reported 5-year mean prior to 1989)
+    //after 1989 with accompanying 5-year averages
+    private static HashMap<String, ArrayList<Integer>> significantImprovement;
+
+    //list of countries who show significant unimprovement (+5% from lowest reported 5-year mean prior to 1989)
+    //after 1989 with acompanying 5-year averages
+    private static HashMap<String, ArrayList<Integer>> significantUnimprovement;
+
+    //list of countries who showed no significant change in data after 1989 ratification
+    private static ArrayList<String> noSignificantChange;
 
     public static void main(String[] args) throws FileNotFoundException {
 
+        //initialize lists and datasets
         countryMap = new HashMap<>();
         nonReporters = new ArrayList<>();
+        significantUnimprovement = new HashMap<>();
+        significantImprovement = new HashMap<>();
+        noSignificantChange = new ArrayList<>();
 
+        //reads data from maleData.csv into countryMap
         Scanner maleData = new Scanner(new File("databases/maleData.csv"));
-        maleData.useDelimiter(",");
+        maleData.useDelimiter(","); //separates values in dataset by comma
 
-        //date to compare before and after is 1989
-
-        while (maleData.hasNext()) {
+        while (maleData.hasNext()) { //loops through the data
             String country = "";
             ArrayList<Integer> tempList = new ArrayList<>();
-            for (int i = 0; i < 52; i++) {
+            for (int i = 0; i < 52; i++) { //loops through the dataset for each country
                 String thisLine = maleData.next();
-                if (thisLine.isEmpty()) {
+                if (thisLine.isEmpty()) { //fills -1 in map when no data is reported
                     tempList.add(-1);
                     continue;
                 }
@@ -36,17 +53,38 @@ public class Main {
                     tempList.add(stringToInt(thisLine));
                 }
             }
-            countryMap.put(country, tempList);
+            countryMap.put(country, tempList); //adds each country and its reported data into countryMap
         }
 
+        //closes the file after data has been read
         maleData.close();
 
+        //calls helper method to replace data in countryMap with 5-year averages
         recordData();
 
+        //removes countries that do not report data before 1989 from countryMap and
+        //adds them to the nonReporters list;
         filterReporters();
 
+        //splits countryMap into 3 separate datasets, denoting countries that have showed significant improvement
+        //since 1989, who have significantly worsened since 1989, and who have shown no major change.
+        findSignificantChange();
+
+        //TESTERS
         //nonReporters.forEach(a -> System.out.println(a));
-        countryMap.forEach((a, b) -> System.out.println(a + " " + b));
+        //countryMap.forEach((a, b) -> System.out.println(a + " " + b));
+        System.out.println("Countries Who Have Shown Improvement: " + significantImprovement.size() + " Countries");
+        System.out.println();
+        significantImprovement.forEach((a, b) -> System.out.println(a + " " + b));
+
+
+       System.out.println("Significant Unimprovement: " + significantUnimprovement.size() + " Countries");
+//        System.out.println();
+//        significantUnimprovement.forEach((a,b) -> System.out.println(a + " " + b));
+
+        System.out.println("Number of countries that showed no significant change: " + noSignificantChange.size());
+//        System.out.println("List:");
+//        noSignificantChange.forEach(System.out::println);
     }
 
     public static int stringToInt(String number) {
@@ -132,7 +170,45 @@ public class Main {
 
 
     private static void findSignificantChange() {
+        //iterates through every item in countryMap;
+        for (Map.Entry<String, ArrayList<Integer>> entrySet : countryMap.entrySet()) {
+            ArrayList<Integer> dataSet = entrySet.getValue(); //assigns each country's data to a local variable
+
+            int min = -1;
+            int max = -1; //temporary assingment of min to negative value
+            final double percentDiff = 0.05; //final int - denotes the percent change we are looking for from the min
+            //(5%)
+            double significantGap; //initializes variable which will be used to store to quantity of 5% from the min
+            // for each country
+
+            for (int i = 0; i < dataSet.size(); i++) {
+                if (i == 0) {//assigns the min to the first element in the data
+                    max = dataSet.get(i);
+                    min = dataSet.get(i);
+                } else if (i < 4) { //assigns min to the lowest of the first 4 values (values before 1989)
+                    if (dataSet.get(i) > max)
+                        max = dataSet.get(i);
+                    if (dataSet.get(i) < min)
+                        min = dataSet.get(i);
+                } else {
+                    significantGap = percentDiff * max; //calculates the difference from the min we are looking for
+
+                    if (dataSet.get(i) <= min - significantGap) { //a country has made significant improvement
+                        significantImprovement.put(entrySet.getKey(), dataSet); //adds improved countries to dataset
+                    } else if (dataSet.get(i) >= max + significantGap) {//a country has significantly worsened since
+                        significantUnimprovement.put(entrySet.getKey(), dataSet);
+                    } else {
+                    }//no significant change detected
+                }
+            }
+        }
+
+        //adds all countries who were not determined to have significant change to noSignificantChange list
+        for (Map.Entry<String, ArrayList<Integer>> entrySet : countryMap.entrySet()) {
+            if (!significantImprovement.containsKey(entrySet.getKey()) &&
+            !significantUnimprovement.containsKey(entrySet.getKey()))
+                noSignificantChange.add(entrySet.getKey());
+        }
 
     }
-
 }
